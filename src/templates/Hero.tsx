9b9fc8b851fbb999f80427da4098572cc511825d
Will <willzhaoshihao@gmail.com>
2021-12-03 00:33:41 +0800

    User darker grey for footer and hero background

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -8,7 +8,7 @@ import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
 import { Logo } from './Logo';
 
 const Hero = () => (
-  <Background color="bg-gray-100">
+  <Background color="bg-gray-300">
     <Section yPadding="py-6">
       <NavbarTwoColumns logo={<Logo xl />}>
         {/* <li>
