266d215e7d4a8394e7ca180137a3ad51841f666f
Han Zhao <windzhaohan@gmail.com>
2021-12-06 15:14:20 +0100

    Update footer

--- src/footer/FooterCopyright.tsx
+++ src/footer/FooterCopyright.tsx
@@ -2,7 +2,7 @@ import { AppConfig } from '../utils/AppConfig';
 
 const FooterCopyright = () => (
   <div className="footer-copyright">
-    © Copyright {new Date().getFullYear()} {AppConfig.title}.
+    © Copyright {new Date().getFullYear()} {AppConfig.site_name}.
     {/* Powered with{' '}
     <span role="img" aria-label="Love">
       ♥
