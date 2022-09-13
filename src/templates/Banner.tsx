d7dcc46dc3710958ac454ce82e88fda07fc54e9e
wxd56987 <672514143@qq.com>
2022-09-13 18:59:08 +0800

    feat: update website new version

--- src/templates/Banner.tsx
+++ src/templates/Banner.tsx
@@ -1,23 +1,44 @@
-// import Link from 'next/link';
+import { FC } from "react";
+import Logo2Svg from "../svg/logo2";
 
-// import { Button } from '../button/Button';
-// import { CTABanner } from '../cta/CTABanner';
-import { Section } from '../layout/Section';
+type Prop = {
+  handleGoLink: (url: string) => void
+}
 
-const Banner = () => (
-  <Section>
-    {/* <CTABanner
-      title="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-      subtitle="Start your Free Trial."
-      button={
-        <Link href="https://creativedesignsguru.com/category/nextjs/">
-          <a>
-            <Button>Get Started</Button>
-          </a>
-        </Link>
-      }
-    /> */}
-  </Section>
-);
+const Banner: FC<Prop> = ({ handleGoLink }) => {
+  return (
+    <div className="pl-body-p pr-body-p">
+      <div
+        className="pl-body-p pr-body-p flex box-border items-center justify-between"
+        style={{
+          height: '16.375rem', padding: '0 3.125rem', background: '#302682',
+          borderRadius: '20px', marginBottom: '6.25rem'
+        }}
+      >
+        <div
+          className="box-border flex items-start justify-start"
+        >
+          <Logo2Svg />
+          <div
+            style={{ marginLeft: '3.125rem' }}
+          >
+            <div className="text-25 text-white font-bold mb-6">Start now to discover ads in metaverse!</div>
+            <div className="flex justify-start items-center">
+              <div className="key-word">Trustworthy</div>
+              <div className="mr-6 ml-6 w-px" style={{ height: '1.125rem', background: 'rgba(255, 255, 255, 0.16)' }}></div>
+              <div className="key-word">Innovative</div>
+              <div className="mr-6 ml-6 w-px" style={{ height: '1.125rem', background: 'rgba(255, 255, 255, 0.16)' }}></div>
+              <div className="key-word">Humanistic</div>
+            </div>
+          </div>
+        </div>
+        <div 
+          className="bg-blue-100 pl-6 pr-6 pt-3.5 pb-3.5 text-white font-semibold rounded-full text-base	cursor-pointer"
+          onClick={() => handleGoLink('https://app.admeta.network/')}
+        >Start now</div>
+      </div>
+    </div>
+  )
+}
 
-export { Banner };
+export default Banner;
